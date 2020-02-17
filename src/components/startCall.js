aff1b0ce7cad21a5aa6e258da6dcfd18b741ebd3
ibtehajaws <ibtehaja@amazon.com>
2020-02-17 17:38:17 +0100

    master

--- src/components/startCall.js
+++ src/components/startCall.js
@@ -1,15 +1,7 @@
 import React from "react";
-import TabPane, { Pane } from "react-split-pane";
 
 const startCall = () => {
-  return (
-    <p>
-      <TabPane split="vertical" minSize={50} defaultSize={100}>
-        <Pane>This is a Pane</Pane>
-        <Pane>This is another Pane,</Pane>
-      </TabPane>
-    </p>
-  );
+  return <p></p>;
 };
 
 export default startCall;
